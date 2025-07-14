111f0e03ac028d38a01ef601445f176bb3ce8977
Anisa Nurani <nurania152@gmail.com>
2025-07-14 10:45:40 -0400

    I added to dos and indicated importance using an emoji and allowed for the deletion of to dos.

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftData
 struct ContentView: View {
     @State private var showNewTask = false
     @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
     var body: some View {
         VStack {
             HStack {
@@ -32,16 +33,30 @@ struct ContentView: View {
             .padding()
             Spacer()
             List {
-                ForEach(toDos) {toDoItem in Text(toDoItem.title)
+                ForEach(toDos) {toDoItem in
+                    if toDoItem.isImportant {
+                        Text("‼️" + toDoItem.title)
+                    } else {
+                        Text(toDoItem.title)
+                    }
                 }
+                .onDelete(perform: deleteToDo)
             }
+            .listStyle(.plain)
         }
         if showNewTask {
-            NewToDoView()
+            NewToDoView(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
+        }
+    }
+    func deleteToDo(at offsets: IndexSet) {
+        for offset in offsets {
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
         }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: ToDoItem.self, inMemory: true)
 }
--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -6,18 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct NewToDoView: View {
+    @Binding var showNewTask: Bool
+    @Bindable var toDoItem: ToDoItem
+    @Environment(\.modelContext) var modelContext
     var body: some View {
         VStack {
             Text("Task title:")
                 .font(.title2)
                 .fontWeight(.bold)
-            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+            TextField("Enter the task description...", text: $toDoItem.title, axis: .vertical)
+            Toggle(isOn: $toDoItem.isImportant) {
                 Text("Is it important?")
                 Button{
-                
+                addToDo()
+                    showNewTask = false
                 } label: {
                     Text("Save")
                 }
@@ -25,8 +30,12 @@ struct NewToDoView: View {
         }
         .padding()
     }
+    func addToDo() {
+        let toDo = ToDoItem(title:toDoItem.title, isImportant: toDoItem.isImportant)
+        modelContext.insert(toDo)
+    }
 }
 
 #Preview {
-    NewToDoView()
+    NewToDoView(showNewTask: .constant(false), toDoItem: ToDoItem(title: "", isImportant: false))
 }
